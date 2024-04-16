bd7730dd8b7adea9d7feec5a188e253238d70fb3
idrissirdi <145381922+idrissirdi@users.noreply.github.com>
2024-04-16 13:31:38 -0400

    Add files via upload

---
+++ Puzzle 2.py
@@ -0,0 +1,14 @@
+#Idris Sampson
+#4/16/24
+#main function the begging of the end for our fearless acheologist
+import random
+while True:
+   puzzle2= random.randint(1, 100)
+   if puzzle2 == 55:
+      break
+print("You, Todd and Stephanie come across a long room, you can see a lever on the other end of the room and you look at the ground")
+print("Todd points out that those are definatly boobie traps and yall should proceed with cation then a ghostly figure of a snake decends from the heavens")
+print("Then the snake says in a heavenly voice \"Anwser my questions correctly and you get to move on\" ")
+print("Stephanie looks prepared and ready then yells \" bring it on you stinky gucci bag\" ")
+Q1 = input("Question 1) what part of the random module is inclusive:")
+   
---
+++ Rand event.py
@@ -0,0 +1,4 @@
+import random
+while True:
+   specail_event= random.randrange(1, 100000000000000000000000000000000000)
+
